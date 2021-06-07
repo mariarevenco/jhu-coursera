c71ca5c1d6d76f9916047da79ab5323ec706ad91
mariarevenco <revenco.maria10@gmail.com>
2021-06-07 07:05:05 +0100

    4th assignment

--- module-4/SpeakHello.js
+++ module-4/SpeakHello.js
@@ -22,11 +22,10 @@
 
 
 (function(window) {
-	var speakWord = "Hello";
-	var helloSpeaker = function (name) {
-		console.log(speakWord + " " + name);
-	}
-
-	window.helloSpeaker = helloSpeaker;
-
+    var helloSpeaker = new Object();
+    var speakWord = "Hello";
+    helloSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    window.helloSpeaker = helloSpeaker;
 })(window);
